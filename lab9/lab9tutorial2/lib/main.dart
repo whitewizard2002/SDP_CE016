fad6669a57a548a582f5d1d857fc55b51d49cefe
Aadit Harshal Baldha <70932466+whitewizard2002@users.noreply.github.com>
2022-09-19 20:59:41 +0530

    Add files via upload

---
+++ lab9/lab9tutorial2/lib/main.dart
@@ -0,0 +1,20 @@
+//codetest1
+/*import 'package:flutter/material.dart';
+import 'package:lab9tutorial2/pages/home.dart';
+void main() => runApp(MaterialApp(
+  home: Home(),
+));*/
+
+//codetest2
+/*import 'package:flutter/material.dart';
+import 'package:lab9tutorial2/pages/home.dart';
+import 'package:lab9tutorial2/pages/loading.dart';
+import 'package:lab9tutorial2/pages/choose_location.dart';
+void main() => runApp(MaterialApp(
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    }
+));*/
---
+++ lab9/lab9tutorial2/lib/pages/choose_location.dart
@@ -0,0 +1,36 @@
+//codetest1
+/*import 'package:flutter/material.dart';
+class ChooseLocation extends StatefulWidget {
+// const ChooseLocation({Key? key}) : super(key: key);
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+class _ChooseLocationState extends State<ChooseLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('CHOOSE LOCATION SCREEN'),
+    );
+  }
+}*/
+
+/*import 'package:flutter/material.dart';
+class ChooseLocation extends StatefulWidget {
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: Text("CHOOSE LOCATION"),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Text('CHOOSE LOCATION SCREEN'),
+    );
+  }
+}*/
