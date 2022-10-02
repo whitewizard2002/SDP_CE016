1686af8777e12385c9aa8a18ef42e0d00d171b84
Aadit Harshal Baldha <70932466+whitewizard2002@users.noreply.github.com>
2022-10-02 19:25:33 +0530

    Add files via upload

---
+++ Lab12/lab12tutorial1/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:lab12tutorial1/pages/choose_location.dart';
+import 'package:lab12tutorial1/pages/home.dart';
+import 'package:lab12tutorial1/pages/loading.dart';
+void main() => runApp(MaterialApp(initialRoute: '/', routes: {
+  '/': (context) => Loading(),
+  '/home': (context) => Home(),
+  '/location': (context) => ChooseLocation(),
+}));
